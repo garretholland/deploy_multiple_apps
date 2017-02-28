b881f8f77eee6d5921faa6a05b8016b407748ec4
Ubuntu <ubuntu@ip-172-31-31-66.us-west-2.compute.internal>
2017-02-28 23:41:10 +0000

    pushing changes to master from AWS

--- multiple_apps/settings.py
+++ multiple_apps/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sjqk6ymu2t!iz2p960gyhh3^44ljr2a40bgjk!-2*fkkl)cdv#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.202.4.25']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
